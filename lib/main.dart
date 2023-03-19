83f738eef49efe3608af0f366db6c8ea08bd3b0b
reznik-gev <reznikgev@gmail.com>
2023-03-19 22:18:15 +0200

    feat: refactor the word pair into a colored Card and centralize it, along with some deprecation of redundant text and semantics, according to https://codelabs.developers.google.com/codelabs/flutter-codelab-first#5

--- lib/main.dart
+++ lib/main.dart
@@ -38,21 +38,53 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-          Text(appState.current.asCamelCase),
-          ElevatedButton(
-            onPressed: () {
-              print('Button was pressed!');
-              appState.getNext();
-            },
-            child: Text('Next'),
-          )
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () {
+                print('Button was pressed!');
+                appState.getNext();
+              },
+              child: Text('Next'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!
+        .copyWith(color: theme.colorScheme.onPrimary);
+
+    return Card(
+      color: theme.colorScheme.primary,
+      elevation: 5.0,
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
