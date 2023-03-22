0e5902c216b1a97d98c5b311b573fedbc4901d3f
reznik-gev <reznikgev@gmail.com>
2023-03-22 23:01:18 +0200

    feat: add FavoritesPage, mostly according to https://codelabs.developers.google.com/codelabs/flutter-codelab-first#8

--- lib/main.dart
+++ lib/main.dart
@@ -61,7 +61,7 @@ class _MyHomePageState extends State<MyHomePage> {
         selectedPage = GeneratorPage();
         break;
       case 1:
-        selectedPage = Placeholder();
+        selectedPage = FavoritesPage();
         break;
       default:
         throw UnimplementedError('No widget for $selectedIndex');
@@ -147,13 +147,82 @@ class GeneratorPage extends StatelessWidget {
   }
 }
 
-class BigCard extends StatelessWidget {
-  const BigCard({
+class FavoritesPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var theme = Theme.of(context);
+    var style;
+
+    if (appState.favorites.isEmpty) {
+      return Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(Icons.favorite_outline_sharp),
+              Text('No favorites yet.'),
+            ],
+          ),
+        ],
+      ));
+    }
+
+    var listViewTiles = <Widget>[];
+
+    listViewTiles.add(Padding(
+      padding: const EdgeInsets.all(20),
+      child: Center(
+        child: Text('You have ${appState.favorites.length} favorite pairs:'),
+      ),
+    ));
+
+    listViewTiles.addAll(
+      appState.favorites
+          .map(
+            (favoritePair) => ListTile(
+              leading: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [for (var i = 0; i < 5; i++) Icon(Icons.favorite)],
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [for (var i = 0; i < 5; i++) Icon(Icons.favorite)],
+              ),
+              title: Center(
+                child: Text(favoritePair.asLowerCase),
+              ),
+            ),
+          )
+          .toList(),
+    );
+
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TitleCard('Favorites'),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView(children: listViewTiles),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleCard extends StatelessWidget {
+  const TitleCard(
+    this.text, {
     super.key,
-    required this.pair,
+    this.padding = 8.0,
   });
 
-  final WordPair pair;
+  final String text;
+  final double padding;
 
   @override
   Widget build(BuildContext context) {
@@ -165,7 +234,37 @@ class BigCard extends StatelessWidget {
       color: theme.colorScheme.primary,
       elevation: 5.0,
       child: Padding(
-        padding: const EdgeInsets.all(20.0),
+        padding: EdgeInsets.all(padding),
+        child: Text(
+          text,
+          style: style,
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+    this.padding = 20.0,
+  });
+
+  final WordPair pair;
+  final double padding;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!
+        .copyWith(color: theme.colorScheme.onPrimary);
+
+    return Card(
+      color: theme.colorScheme.primary,
+      elevation: 5.0,
+      child: Padding(
+        padding: EdgeInsets.all(padding),
         child: Text(
           pair.asLowerCase,
           style: style,
